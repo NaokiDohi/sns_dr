17e9ed9fd3eb61daf47ccb8f8af840e6b9dad179
NaokiDohi <1210113.naoki.dohi.913@gmail.com>
2021-01-03 23:02:07 +0900

    create custom user

--- back/config/settings.py
+++ back/config/settings.py
@@ -121,7 +121,9 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
+AUTH_USER_MODEL = 'core.User'
 
 STATIC_URL = '/static/'
+
 MEDIA_ROOT = os.path.join(BASE_DIR, '_media')
 MEDIA_URL = '/media/'
